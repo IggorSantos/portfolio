509bb20e74164c7c94590429c70cfe177e589984
Igor Santos <iggor.jsantos@gmail.com>
2023-12-16 20:20:21 -0300

    adding component header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponentsModule } from './components/shared-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

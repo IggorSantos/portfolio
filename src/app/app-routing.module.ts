016f7ba6f9449998c4192e055d6989481eccc217
Igor Santos <iggor.jsantos@gmail.com>
2023-12-20 21:47:59 -0300

    adding link to page projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren:() => import('./pages/home/home.module').then(m => m.HomeModule) }
+  {path: '', loadChildren:() => import('./pages/home/home.module').then(m => m.HomeModule) },
+  {path: 'projects', loadChildren:() => import('./pages/projects/projects.module').then(m => m.ProjectsModule) }
 ];
 
 @NgModule({

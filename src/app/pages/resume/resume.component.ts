1f4e76227cbc8688ab23fa7d8e8fbfcdddfe30ab
Igor Santos <iggor.jsantos@gmail.com>
2023-12-23 18:08:12 -0300

    adding resume page

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
+  educations = [
+    {"title": "Tecnólogo em Analise e Desenvolvimento de Sistemas",
+    "local": "IFBA - Campus Irecê-Bahia",
+    "date": "2015-2020"
+    },
+    {"title": "Ensino Médio",
+    "local": "Escola Polivalente de Irecê",
+    "date": "2010-2012"
+    },
+  ]
 
   constructor() { }
 
--- src/app/pages/resume/resume.module.ts
+++ src/app/pages/resume/resume.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ResumeRoutingModule } from './resume-routing.module';
 import { ResumeComponent } from './resume.component';
 
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
+
 
 @NgModule({
   declarations: [ResumeComponent],
   imports: [
     CommonModule,
-    ResumeRoutingModule
+    ResumeRoutingModule,
+    SharedComponentsModule
   ]
 })
 export class ResumeModule { }
